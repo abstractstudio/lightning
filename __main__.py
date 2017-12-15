749c059574e4b5827e2b810b8dc786b581a95515
Noah Kim <noahbkim@gmail.com>
2017-12-15 10:44:05 -0500

    Cyclic check not working

--- __main__.py
+++ __main__.py
@@ -1,3 +1,5 @@
+#!/usr/local/bin/python3
+
 """A lightweight Javascript concatenator.
 
 Lightning is an fast and efficient Javascript concatenator that
@@ -11,11 +13,7 @@ import glob
 import time
 
 
-DEV = False
-if DEV:
-    os.chdir("..")
-
-INDEX = os.path.join(os.getcwd(), "lightning.conf")
+INDEX = os.path.join(os.getcwd(), ".lightning.conf")
 HEAD = "@head"
 REQUIRE = "@require"
 PROVIDE = "@provide"
@@ -24,56 +22,60 @@ PROVIDE = "@provide"
 class Source:
     """Javascript source file container."""
 
-    def __init__(self, path:str):
+    def __init__(self, path: str):
         """Create a source file container."""
 
         self.path = os.path.abspath(path)
+        self.code = None
         self.head = False
         self.require = []
         self.provide = []
-        self.code = None
         self.search()
 
-    def __getitem__(self, target:str):
+    def __getitem__(self, target: str):
         """Get the require or provide lists."""
 
-        if target == REQUIRE:
-            return self.require
-        elif target == PROVIDE:
-            return self.provide
+        return {REQUIRE: self.require, PROVIDE: self.provide}[target]
 
     def __repr__(self):
         """Represent the source file as a string."""
 
         return "Source[" + os.path.basename(self.path) + "]"
 
-    def search(self):
+    def search(self, cache=True):
         """Search for require and provide statements."""
 
-        with open(self.path) as file:
-            code = file.read()
-        self.code = code
-        if code.find(HEAD) > -1:
+        # Read the file
+        self.read(cache=cache)
+
+        # Check if this is the head
+        if self.code.find(HEAD) > -1:
             self.head = True
             return
+
+        # Iterate search items
         for target in (REQUIRE, PROVIDE):
             start = 0
             out = set()
             length = len(target)
+            
             while True:
-                index = code.find(target, start)
+                index = self.code.find(target, start)
                 if index < 0:
                     break
-                newline = code.find("\n", index)
-                separated = code[index+length+1:newline].split(",")
+                newline = self.code.find("\n", index)
+                separated = self.code[index+length+1:newline].split(",")
                 out |= set(map(lambda x: x.strip(), separated))
                 start = newline + 1
+
+            # Clear and set the search
             self[target].clear()
             self[target].extend(out)
 
-    def read(self, cache=True):
+    def read(self, cache=False):
         """Read the contents of the file."""
 
+        # Read if not cached
         if self.code is None:
             with open(self.path) as file:
                 code = file.read()
@@ -82,7 +84,7 @@ class Source:
         return self.code
 
 
-def common(paths:[str]) -> str:
+def common(paths: [str]) -> str:
     """Return the common path of a list with wildcard."""
 
     path = os.path.abspath(os.path.commonpath(paths))
@@ -105,7 +107,7 @@ def find(require, sources):
 class Target:
     """A set of sources defined by an index file."""
 
-    def __init__(self, target:str, include:[str], exclude:[str]):
+    def __init__(self, target: str, include: [str], exclude: [str]):
         """Initialize a build with its target and source paths."""
 
         self.target = target
@@ -131,20 +133,27 @@ class Target:
             files -= set(glob.glob(path, recursive=True))
         self.sources.extend(map(Source, files))
 
-    def includes(self, path:str) -> bool:
+    def includes(self, path: str) -> bool:
         """Check if a source file is in the build include."""
 
-        return (os.path.abspath(path) in
-                tuple(map(lambda s: s.path, self.sources)))
+        path = os.path.abspath(path)
+        for source in self.sources:
+            if source.path == path:
+                return True
+        return False
 
     def sort(self):
         """Sort the sources topologically."""
 
         correct = []
+
+        # Put head in the front of the correct list
         for source in self.sources:
             if source.head:
                 self.sources.remove(source)
                 correct.append(source)
+
+        # Do the algorithm
         while self.sources:
             cyclic = True
             for source in self.sources:
@@ -155,10 +164,14 @@ class Target:
                     cyclic = False
                     self.sources.remove(source)
                     correct.append(source)
+
+            # Break if there are cyclic dependencies
             if cyclic:
                 raise RuntimeError(
                     "Circular dependencies found in {}.".format(
                     os.path.basename(source.path)))
+
+        # Save correct ordering
         self.sources = correct
 
     def build(self):
@@ -166,19 +179,20 @@ class Target:
 
         self.index()
         self.sort()
+        print(self.sources)
         with open(self.target, "w") as file:
             for source in self.sources:
                 file.write(source.read().rstrip() + "\n\n\n")
-        print("Built", repr(self))
+        print("Built {}".format(repr(self)))
                     
 
-def sep(path:str) -> str:
+def sep(path: str) -> str:
     """Fix a path according to the operating system."""
 
     return path.replace("/", os.sep)
         
 
-def index(path:str=INDEX) -> [Target]:
+def index(path: str=INDEX) -> [Target]:
     """Get the build sources from the index file."""
 
     out = []
@@ -186,9 +200,13 @@ def index(path:str=INDEX) -> [Target]:
         target = None
         include = None
         exclude = None
-        for line in file.readlines():
+
+        # Iterate the file
+        for line in file:
             line = line.lstrip()
             first = line[0]
+
+            # Check command
             if line == "" or first in "#;":
                 continue
             elif first in "+-" and target is None:
@@ -197,20 +215,26 @@ def index(path:str=INDEX) -> [Target]:
                 include.append(sep(line[1:].strip()))
             elif first == "-":
                 exclude.append(sep(line[1:].strip()))
+
+            # Add as a new target
             else:
                 if include or exclude:
                     out.append(Target(target, include, exclude))
                 target = line.strip()
                 include = []
                 exclude = []
+
+        # Add the last target since this is like do while
         out.append(Target(target, include, exclude))
+
+    # Return
     return out
 
 
 class NaiveDeltaHandler(watchdog.events.FileSystemEventHandler):
     """Calls a full sort and concatenate when the filesystem changes."""
 
-    def __init__(self, target:[Target]):
+    def __init__(self, target: [Target]):
         """Initialize the file handler with its config path."""
 
         self.target = target
@@ -241,12 +265,21 @@ class NaiveDeltaHandler(watchdog.events.FileSystemEventHandler):
 
 
 if __name__ == "__main__":
+    """This is run if someone calls lightning from the command line."""
+
     import sys
+    path = INDEX
+    if len(sys.argv) > 1:
+        path = sys.argv[1]
+
+    # Create the observer
     observer = watchdog.observers.Observer()
-    for target in index():
+    for target in index(path=path):
         handler = NaiveDeltaHandler(target)
         observer.schedule(handler, target.common, recursive=True)
     observer.start()
+
+    # Wait for kill command
     while True:
         try:
             time.sleep(0.5)
